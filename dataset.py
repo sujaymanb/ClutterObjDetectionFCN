9d1615512df4fd065fabb5e90cae8ee5dba9197a
sujaymanb <sujaymanb@gmail.com>
2020-12-25 16:21:37 -0500

    added transforms

--- dataset.py
+++ dataset.py
@@ -8,12 +8,17 @@ import numpy as np
 # TODO: validation
 class SuctionData(data.Dataset):
     def __init__(self, root_dir, mode='train', transform=None):
+        """
+        args:
+            root_dir: root directory of path
+            mode: load train or test dataset
+            transform: apply any transforms on the data or None
+        """
         self.root_dir = root_dir
         self.mode = mode
         self.transform = transform
 
         # load file list
-        #print(os.path.join(root_dir, 'data', self.mode, 'color', '*'))
         self.color_list = glob.glob(os.path.join(root_dir, 'data', self.mode, 'color', '*.png'))
         self.depth_list = glob.glob(os.path.join(root_dir, 'data', self.mode, 'depth', '*.png'))
         self.label_list = glob.glob(os.path.join(root_dir, 'data', self.mode, 'label', '*.png'))
@@ -30,17 +35,22 @@ class SuctionData(data.Dataset):
             idx = idx.tolist()
 
         color = utils.rgb2array(self.color_list[idx],hwc=False)
+        color = torch.from_numpy(color)
         depth = utils.depth2array(self.depth_list[idx],hwc=False)
+        depth = torch.from_numpy(depth)
+        if self.transform:
+            color = self.transform(color)
+            depth = self.transform(depth)
         if self.mode == 'train':
             label = utils.label2array(self.label_list[idx],hwc=False)
-            sample = {'color': torch.from_numpy(color),
-                      'depth': torch.from_numpy(depth),
-                      'label': torch.from_numpy(label.astype(np.float32))}
+            label = torch.from_numpy(label.astype(np.float32))
+            if self.transform:
+                label = self.transform(label)
+            sample = {'color': color,
+                      'depth': depth,
+                      'label': label}
         else:
-            sample = {'color': torch.from_numpy(color),
-                      'depth': torch.from_numpy(depth)}
-
-        if self.transform:
-            sample = self.transform(sample)
+            sample = {'color': color,
+                      'depth': depth}
 
         return sample
